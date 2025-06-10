c0f9b8fb83cb3513e7611f1aee25e76e2631e4c7
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-10 09:00:11 +0500

    Bajarildi

--- program05.py
+++ program05.py
@@ -3,3 +3,11 @@ price = 2500                #Mahsulot narxlari jadvali (1 donadan 10 tagacha)
 for i in range(1,11):
     reslut = i*price
     print(f"{i} dona={reslut}")
+
+#     Misol:
+
+# ```
+# 1 dona = 2500
+# 2 dona = 5000
+# ...
+# 10 dona = 25000
